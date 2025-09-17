0b56331da01eac9e6f283b42e48256853fffff6f
dhamsilver <dhamsilver58@gmail.com>
2025-09-16 23:47:00 -0500

    Se inicializa el proyecto

---
+++ src/main/java/com/edu/upeu/pe/samanacafe/SamanaCafeApplication.java
@@ -0,0 +1,13 @@
+package com.edu.upeu.pe.samanacafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamanaCafeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamanaCafeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/upeu/pe/samanacafe/SamanaCafeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.upeu.pe.samanacafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamanaCafeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
